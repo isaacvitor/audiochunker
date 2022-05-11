26cfe9a55631de7aa8b7786ffbb7d9914a25b12f
isaac.silva <isaacvitor@gmail.com>
2022-05-12 00:02:19 +0100

    Added chunker using ffmpeg

--- audiochunker/main.py
+++ audiochunker/main.py
@@ -1,7 +1,11 @@
+import logging
 import os
 import subprocess
 from tempfile import mkstemp
+from typing import List
 
+
+logger = logging.getLogger('scribezilla-service')
 
 class FFMPEGTools:
     """
@@ -74,3 +78,88 @@ class FFMPEGTools:
             raise e
         finally:
             os.remove(silence_path_file)
+
+
+class AudioChunk:
+    def __init__(self, file: str = None, file_size: str = None, start: int = 0, end: int = 0, duration: float = 0, text: str = None):
+        self.file = file
+        self.file_size = file_size
+        self.start = start
+        self.end = end
+        self.duration = duration
+        self.text = text
+        self.conf = 1.0
+    
+    def __repr__(self):
+        return f'file:{self.file} size:{self.file_size} start:{self.start} end:{self.end} duration:{self.duration} text:{self.text}'
+
+
+class AudioFileChunker:
+    def __init__(self, input_file_path=None, chunks_path=None, chunk_suffix='chunk_', silence_threshold=-30, silence_duration=0.5):   
+        self.input_file_path = input_file_path
+        self.chunks_path = chunks_path
+
+        self.chunk_suffix = chunk_suffix
+        self.silence_threshold = silence_threshold
+        self.silence_duration = silence_duration
+
+        self.chunks: List[AudioChunk] = []
+        self.silences = []
+        self.commands = []
+        self.big_command = ''
+        self.silence_threshold = silence_threshold
+        self.silence_duration = silence_duration
+        
+    def __get_chunk_size(self):
+        try:
+            for chunk in self.chunks:
+                if os.path.exists(chunk.file):
+                    chunk.file_size = os.path.getsize(chunk.file)
+                else:
+                    logger.warning(f'File {chunk.file} does not exist')
+        except Exception as e:
+            raise e 
+
+
+    def chunking(self,input_file_path = None, chunks_path=None, chunk_suffix=None):
+        
+        if input_file_path is None and self.input_file_path is None:
+            raise Exception('input_file_path was not specified')
+        input_file_path = self.input_file_path if input_file_path is None else input_file_path
+        
+        if not os.path.isfile(input_file_path):
+            raise FileNotFoundError('input_file_path must be a audio file')
+
+        if chunks_path is None and self.chunks_path is None:
+            raise ValueError('chunks_path was not specified and must be a directory')
+            
+        
+        chunks_path = self.chunks_path if chunks_path is None else chunks_path
+        if not os.path.isdir(chunks_path):
+            raise Exception('chunks_path must be a directory')
+
+        try:
+            # Getting silences times
+            silence_file = FFMPEGTools.create_silence_file(input_file_path, self.silence_threshold, self.silence_duration)
+            self.silences =  FFMPEGTools.get_silences_form_file(silence_file)
+               
+            chunk_suffix = self.chunk_suffix if chunk_suffix is None else chunk_suffix
+            chunk_count = 0
+            for s in range(0, len(self.silences)):
+                tms = self.silences[s:s+2]        
+                if len(tms) == 2:
+                    t1 = tms[0]['end'] - 0.25
+                    t2 = tms[1]['start'] - tms[0]['end'] + 3 * 0.25
+                    chunk_name = f'{chunk_suffix}{chunk_count}.wav'
+                    chunk_path = os.path.join(chunks_path, chunk_name)
+                    __command = f'ffmpeg -v error -y -ss {t1} -t {t2} -i {input_file_path} {chunk_path}'
+                    self.commands.append(__command)
+                    self.big_command += __command + ' & '
+                    self.chunks.append(AudioChunk(file=chunk_path, file_size=0, start=t1, end=t2+t1, duration=t2, text=None))
+                chunk_count += 1
+
+            subprocess.check_output(self.big_command, shell=True)
+            self.__get_chunk_size()
+            return (self.chunks, self.silences)
+        except Exception as e:
+            raise e
