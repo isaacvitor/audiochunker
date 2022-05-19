1804bf94e0e852b797f326d2c672ff32f2272534
Isaac Vitor <isaacvitor@gmail.com>
2022-05-19 08:17:08 +0100

    Lib base (#8)
    * Created lib base setup
    
    * Added new tests

---
+++ audiochunker/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='audiochunker-isaacvitor',
+    packages=find_packages(include=['audiochunker']),
+    version='0.1.0',
+    description='Python library for chunking audio',
+    author='Isaac Vitor',
+    license='MIT',
+    url="https://github.com/isaacvitor/audiochunker",
+    project_urls={
+        "Bug Tracker": "https://github.com/isaacvitor/audiochunker/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['pydub==0.25.1'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==7.1.2'],
+    test_suite='tests',
+)
--- tests/unit/test_ffmpeg.py
+++ tests/unit/test_ffmpeg.py
@@ -3,39 +3,108 @@ import os
 import pytest
 
 from audiochunker.main import (
-    FFMPEGTools
+    FFMPEGTools, FFMPEGException
 )
 
 # This audio file is supposed to have 3 utterances and 4 silence periodes.
 audio_3_utterances = 'tests/resources/audio_3_utterances.wav'
+reference_silence_file = 'tests/resources/silence_reference'
 
 # Inexistent audio file
 inexistent_audio = 'tests/resources/inexistent.wav'
 
+# Invalid wav file
+invalid_wav = 'tests/resources/error.wav'
+
 def test_with_inexistent_audio_file():    
     with pytest.raises(FileNotFoundError):
-        silence_file = FFMPEGTools.create_silence_file(inexistent_audio)
+        FFMPEGTools.create_silence_content(inexistent_audio)
 
 def test_with_inexistent_silence_file():    
-    with pytest.raises(FileNotFoundError):
-        silence_file = FFMPEGTools.get_silences_form_file(inexistent_audio)
+    with pytest.raises(ValueError):
+        FFMPEGTools.get_silences_from_file(None)
 
+def test_invalid_silence_file():    
+    with pytest.raises(FFMPEGException):
+        FFMPEGTools.get_silences_from_file(inexistent_audio)
+
+def test_no_silence_threshold():    
+    with pytest.raises(ValueError):
+        FFMPEGTools.create_silence_content(audio_3_utterances, silence_threshold=None)
+
+def test_no_silence_duration():    
+    with pytest.raises(ValueError):
+        FFMPEGTools.create_silence_content(audio_3_utterances, silence_duration=None)
+
+def test_ffmpeg_exception():    
+    with pytest.raises(FFMPEGException):
+        FFMPEGTools.create_silence_content(reference_silence_file)
+
+def test_invalid_wav():    
+    with pytest.raises(FFMPEGException):
+        FFMPEGTools.create_silence_file(invalid_wav)
+
+def test_create_silence_content():
+    audio_path = audio_3_utterances
+    silence_threshold = -30
+    silence_duration = 0.5
+    silence_content = FFMPEGTools.create_silence_content(audio_path, silence_threshold, silence_duration)
+
+    lines = silence_content.splitlines()
+    start_match = False
+    end_match = False
+
+    assert silence_content is not None
+    assert len(silence_content) > 0
+   
+    for line in lines:
+        if 'silence_start' in line:
+            start_match = True
+        if 'silence_end' in line:
+            end_match = True
+    assert start_match
+    assert end_match
 
 def test_create_silence_file():
     audio_path = audio_3_utterances
     silence_threshold = -30
     silence_duration = 0.5
-    silence_file = FFMPEGTools.create_silence_file(audio_path, silence_threshold, silence_duration)
+    silence_file_path = FFMPEGTools.create_silence_file(audio_path, silence_threshold, silence_duration)
 
-    assert silence_file is not None
-    assert os.path.exists(silence_file)
-    os.remove(silence_file)
+    start_match = False
+    end_match = False
+    
+    assert silence_file_path is not None
+    with open(silence_file_path, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            if 'silence_start' in line:
+                start_match = True
+            if 'silence_end' in line:
+                end_match = True
+    assert start_match
+    assert end_match
+    
 
-
-def test_get_silences():
-    silence_file = FFMPEGTools.create_silence_file(audio_3_utterances)
-    silences = FFMPEGTools.get_silences_form_file(silence_file)
+def test_get_silences_file():
+    silence_file_path = FFMPEGTools.create_silence_file(audio_3_utterances)
+    silences = FFMPEGTools.get_silences_from_file(silence_file_path)
+    reference = FFMPEGTools.get_silences_from_file(reference_silence_file)
     
     assert silences != None
     assert len(silences) == 4
-    assert os.path.exists(silence_file) == False
+    assert silences == reference
+
+def test_get_silences_content():
+    silence_content = FFMPEGTools.create_silence_content(audio_3_utterances)
+    silences = FFMPEGTools.get_silences_from_content(silence_content)
+    reference = FFMPEGTools.get_silences_from_file(reference_silence_file)
+    
+    assert silences != None
+    assert len(silences) == 4
+    assert silences == reference
+
+def test_is_a_valid_silence_file():
+    valid_path = FFMPEGTools.create_silence_file(audio_3_utterances)
+
+    assert FFMPEGTools.is_valid_silence_file(valid_path)
