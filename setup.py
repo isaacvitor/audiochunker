e73ad85787ea85879c1af468e18a8372aca20756
isaac.silva <isaacvitor@gmail.com>
2022-07-01 12:03:10 +0100

    Added general information

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='audiochunker',
     packages=find_packages(include=['audiochunker']),
-    version='0.2.0',
+    version='0.2.1',
     description='Python library for chunking audio',
     author='Isaac Vitor',
     license='MIT',
